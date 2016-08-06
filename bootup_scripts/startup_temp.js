84b9c72d9175fda65e205565452125b036156770
root <patrickday9@gmail.com>
2016-08-06 17:40:53 +0000

    Added updated startup temp

--- bootup_scripts/startup_temp.js
+++ bootup_scripts/startup_temp.js
@@ -4,10 +4,10 @@ var exec = require('child_process').exec;
 function puts(error, stdout, stderr) { sys.puts(stdout) }
 
 var recursive = function () {
-    exec("python /home/root/executable/sensor_development/run_temp_v2.py temp_board12 status_board12 board12 1 1 1", puts);
+    exec("python /home/root/sensor_development/run_temp_v3.py temp_ed4 status_ed1 edison4 10 5", puts);
     console.log("Updating Temperature at:");
     var date = new Date();
     console.log(date);
-    setTimeout(recursive, 15000);
+    setTimeout(recursive, 65000);
 }
 recursive();
