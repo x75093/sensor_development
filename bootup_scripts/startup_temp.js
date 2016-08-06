312e065a8d71f587e099661671bd2894724d8458
patrickday9 <pday@g.harvard.edu>
2016-08-06 16:25:11 -0400

    Update startup_temp.js

--- bootup_scripts/startup_temp.js
+++ bootup_scripts/startup_temp.js
@@ -4,10 +4,10 @@ var exec = require('child_process').exec;
 function puts(error, stdout, stderr) { sys.puts(stdout) }
 
 var recursive = function () {
-    exec("python /home/root/sensor_development/run_temp_v3.py temp_ed<board #> status_ed1 edison<board #> 10 5", puts);
+    exec("python /home/root/sensor_development/run_temp_v3.py temp_ed<board #> status_ed1 edison<board #> 12 60", puts);
     console.log("Updating Temperature at:");
     var date = new Date();
     console.log(date);
-    setTimeout(recursive, 65000);
+    setTimeout(recursive, 900000);
 }
 recursive();
